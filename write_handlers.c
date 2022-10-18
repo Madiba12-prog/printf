a1fda28f3489882a8175b1808fa9b0da1f1a538e
Madan <madanbeshir496@gmail.com>
2022-10-18 08:00:38 -0700

    Team

--- write_handlers.c
+++ write_handlers.c
@@ -2,14 +2,13 @@
 
 /************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - Prints a string
+ * handle_write_char - Prints a strinsg
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
- *
  * Return: Number of chars printed.
  */
 int handle_write_char(char c, char buffer[],
