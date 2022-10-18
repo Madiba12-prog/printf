0c4ae5da643fe68c87c32b4c64956a04767cc632
Madan <madanbeshir496@gmail.com>
2022-10-18 07:12:52 -0700

    Team

---
+++ functions.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/************************* PRINT CHAR *************************/
+
+/**
+* print_char - Prints a char
+* @types: List a of argument
+* @buffer: Buffer array to handle print
+ *
+ *     * @flags:  Calculates active flags
+ *
+ *      * @width: Width
+ *
+ *       * @precision: Precision specification
+ *
+ *        * @size: Size specifier
+ *
+ *         * Return: Number of chars printed
+ *
+ *          */
